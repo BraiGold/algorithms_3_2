7d45b4aae16134b9924796e7ae19e49af5db51a9
martinezmanuela <martinez.manuela.22@gmail.com>
2016-05-15 22:29:51 -0300

    exp una nueva esperanza, esperanza de aprobar tenemos que tener (?

---
+++ exp/UnaNuevaEsperanza/exp2/exp2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+LC_NUMERIC="en_US.UTF-8"
+
+iteraciones=1
+n="100"
+m="$(seq 99 500 4950)" 
+# el minimo m que puedo tener dado n es: m = n-1 
+# el maximo m que puedo tener dado n es: m = n * (n - 1) /2 
+
+
+while getopts 'ha:' opt; do
+  case $opt in
+    a) iteraciones=$OPTARG ;;
+    h) echo ""
+       echo "    Experimento 2 - Tiempo de ejecución variando cantidad de pasadizos"
+       echo ""
+       echo "    Opciones disponibles:"
+       echo "        -h        Imprime este texto de ayuda."
+       echo "        -a <núm>  Cantidad de iteraciones - por defecto 1"
+       echo ""
+       exit 0 ;;
+  esac
+done
+
+#genero archivos de entrada
+for i in $m; do
+  echo "Esta creando el archivo numero"
+  printf "%d\n " $i
+  printf "%d %d %d \n" $n $i | $(dirname $0)/generador #parametros del programa generador de archivos de entrada (n, m, pesomin, pesomax) 
+done 
+
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-exp2.txt
+
+for k in $m; do
+  printf "%d " $k >> $(dirname $0)/tiempos-exp2.txt
+  echo "Esta corriendo la instancia numero"
+  printf "%d\n " $k
+  for h in $(seq 1 $iteraciones); do
+    echo "iteracion numero"
+    printf "%d\n " $h
+    $(dirname $0)/../../../UnaNuevaEsperanza < $(dirname $0)/grafoConexo-$k.txt -t >> $(dirname $0)/tiempos-exp2.txt
+  done
+  printf "\n" >> $(dirname $0)/tiempos-exp2.txt
+done
+
+#octave -q $(dirname $0)/exp1.m
---
+++ exp/UnaNuevaEsperanza/exp2/generador.cpp
@@ -0,0 +1,150 @@
+#include <iostream>     //cin cout
+#include <stdio.h>      // printf fopen fclose fputs 
+#include <stdlib.h>     //
+#include <string>       //to_string
+#include <vector>
+#include <algorithm>    // std::sort
+
+using namespace std;
+
+bool yaEsta(int i, int j, vector<pair<pair<int, int>, int> > aristas) {
+	int h = 0;
+	bool loEncontre = false;
+	while(h < aristas.size() && !loEncontre) {
+		if ( (aristas[h].first.first == i && aristas[h].first.second == j) || (aristas[h].first.first == j && aristas[h].first.second == i) ) {
+			loEncontre = true;
+		}
+		h++;
+	}
+	return loEncontre;
+}
+
+int randombis() {
+	return rand() % 1000 + (rand() % 1000) * 1000 + (rand() % 1000) * 1000000;
+}
+
+
+int main() {
+	FILE * doc;
+	
+	int n, m;
+	cin >> n >> m ; 
+	
+	vector<bool> loUse;
+	vector<pair<pair<int,int>, int> > aristas;
+	vector<int> tipo;
+	int especial1;
+	int especial2;
+
+
+	char filename[64];
+	sprintf(filename, "grafoConexo-%d.txt", m);
+	
+
+	doc = fopen(filename,"w");
+
+	if (doc!=NULL) {
+  	
+		fprintf(doc, "%d %d\n", n, m);
+		time_t seconds;
+		time(&seconds);
+		srand((unsigned int) seconds);
+	
+		int i;
+		int j;
+
+		for (int h = 0; h < n; h++) {
+			loUse.push_back(false);
+		}
+		for (int h = 0; h < n; h++) {
+			tipo.push_back(0);
+		}
+
+		i = randombis() % n;
+		j = randombis() % n;
+
+		while (i == j) {
+			j = randombis() % n;
+		}
+
+		loUse[j] = true;
+		loUse[i] = true;
+		
+		especial1 = randombis() %  n;  
+		especial2 = randombis() %  n;  
+		while(especial2 == especial1){
+			especial2 = randombis() %  n;  
+		}
+
+		tipo[especial1] = 1;
+		tipo[especial2] = 1;
+
+		for(int h = 0; h < n; h++){
+			if(h != especial1 && h != especial2){
+				tipo[h] = randombis() % (1 - 0 + 1) + 0;  
+			}
+		}
+
+
+		pair<pair<int, int>, int> arista;
+		arista.first.first = i;
+		arista.first.second = j;
+		arista.second = tipo[aristas.size()];
+		aristas.push_back(arista);		
+
+		for (int h = 1; h < n -1; h++) {
+
+			i = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+			j = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+
+			while (loUse[i] == false) {
+				i = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+			}
+		
+			cerr << "ya tengo un i= " << i << endl; 
+
+			while (loUse[j] == true) {
+				j = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+			}
+
+			cerr << "ya tengo un j= " << j << endl; 
+
+			loUse[j] = true;
+
+			pair<pair<int, int>, int> arista;
+			arista.first.first = i;
+			arista.first.second = j;
+			arista.second = tipo[aristas.size()];
+
+			aristas.push_back(arista);		
+
+		}
+
+		while (aristas.size() < m) {
+			cerr << "estoy buscando una nueva arista, ya tengo: " << aristas.size() << endl;
+			i = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+			j = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+			while (yaEsta(i, j, aristas) || (i == j) ) {
+				i = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+				j = randombis() % n;    //(n - 1 - 0 + 1) + 0   
+			} 
+
+			arista.first.first = i;
+			arista.first.second = j;
+			arista.second = tipo[aristas.size()];
+			aristas.push_back(arista);		
+
+		}
+
+		cerr << "estoy por imprimir" << endl;
+
+		for (int i = 0; i < m ; i++) {
+			fprintf(doc, "%d %d %d\n", aristas[i].first.first, aristas[i].first.second, aristas[i].second);
+		}
+    	
+		cerr << "ya imprimi todo " << endl;
+
+    	fclose(doc);
+	}
+	return 0;
+}
